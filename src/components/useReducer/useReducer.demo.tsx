c137b1e8834ab6c7beaf0dcab9b276f1a5483423
neominal.solutions <neominal.solutions@gmail.com>
2024-09-11 10:31:51 +0300

    UseReduxer

--- src/components/useReducer/useReducer.demo.tsx
+++ src/components/useReducer/useReducer.demo.tsx
@@ -1,50 +1,99 @@
-import { useReducer } from 'react';
+import { useReducer, useState } from 'react';
 import CouponRecuder, {
 	CouponItem,
 	CouponState,
 } from '../../reducers/couponReducer';
 
 function UseReducerDemo() {
-	const initState: CouponState = { total: 0, quantity: 1, times: 1, items: [] };
+	const games: CouponItem[] = [
+		{
+			id: 1,
+			name: 'GS-FB Türkiye Kupası',
+			score: 'MS',
+			rate: 2.1,
+		},
+		{
+			id: 2,
+			name: 'BJK-TS Lig Maçı',
+			score: 'İlk Yarı',
+			rate: 3.2,
+		},
+	];
+
+	const initState: CouponState = {
+		total: 0,
+		quantity: 1,
+		times: 1,
+		items: [],
+		selectedId: -1,
+	};
 
 	const [state, dispatch] = useReducer(CouponRecuder, initState);
+	const [selectedId, setSelectedId] = useState<number>();
 
 	const addItem = () => {
-		const item = {
-			game: 'Ankara Gücü-Galatasaray',
-			score: 'ms',
-			rate: 10.2,
-		};
+		// const item = games.find((x) => x.id === selectedId) as CouponItem;
 
-		state.items.push(item);
+		const item = games.find((x) => x.id === state.selectedId) as CouponItem;
 
-		dispatch({ type: 'ItemAdded', payload: state });
+		console.log('item', item);
+
+		dispatch({ type: 'ItemAdded', payload: item });
 	};
 
 	return (
 		<>
+			<p>Maçlar</p>
+			<select
+				onChange={(e: any) => {
+					console.log('e', e);
+					// setSelectedId(Number(e.target.value));
+					dispatch({
+						type: 'ItemSelected',
+						payload: { selectedId: Number(e.target.value) },
+					});
+				}}
+			>
+				{games.map((item: CouponItem) => {
+					return (
+						<option key={item.id} value={item.id}>
+							{item.name}
+						</option>
+					);
+				})}
+			</select>
+			<hr></hr>
+			<h1>Kuponum</h1>
 			{state.items.map((item: CouponItem, index: number) => {
 				return (
-					<div>
-						{item.game} / {item.score}
+					<div key={item.id}>
+						{item.name} / {item.score}
 					</div>
 				);
 			})}
-			Kupon Fiyat: {state.total}
+			Kupon Fiyat: {state.total.toFixed(2)}
 			<br></br>
+			<h2>İşlemler</h2>
 			<button onClick={addItem}>Ekle</button>
 			<button
 				onClick={() => {
-					state.quantity = 2;
-					const newState = state;
-
 					dispatch({
 						type: 'QuantityChanged',
-						payload: newState,
+						payload: { id: 1, quantity: 2 },
 					});
 				}}
 			>
 				Adet
+			</button>
+			<button
+				onClick={() => {
+					dispatch({
+						type: 'TimesChanged',
+						payload: { id: 1, times: 3 },
+					});
+				}}
+			>
+				Misli
 			</button>
 		</>
 	);
--- src/reducers/couponReducer.ts
+++ src/reducers/couponReducer.ts
@@ -1,10 +1,12 @@
 export type CouponItem = {
-	game: string;
+	id: number;
+	name: string;
 	score: string; // ms, ilk yarı
 	rate: number; // oran
 };
 
 export type CouponState = {
+	selectedId: number;
 	items: CouponItem[];
 	total: number;
 	quantity: number; // adet
@@ -15,10 +17,16 @@ export type CouponActionType =
 	| 'QuantityChanged'
 	| 'TimesChanged'
 	| 'ItemAdded'
-	| 'ItemDeleted';
+	| 'ItemDeleted'
+	| 'ItemSelected';
 
 export type CouponAction = {
-	payload: CouponState;
+	payload:
+		| { selectedId: number }
+		| { id: number; quantity: number }
+		| { id: number; times: number }
+		| CouponItem;
+
 	type: CouponActionType;
 };
 
@@ -28,7 +36,7 @@ function calculateTotal(state: CouponState): number {
 		sum += item.rate;
 	});
 
-    console.log('state.quantity', state.quantity);
+	console.log('state.quantity', state.quantity);
 	let total = sum * state.times * state.quantity;
 	return total;
 }
@@ -37,22 +45,57 @@ function CouponRecuder(state: CouponState, action: CouponAction) {
 	if (action.type === 'QuantityChanged') {
 		console.log('değişim', action.type);
 
+		const payload = action.payload as { id: number; quantity: number };
+
+		state.quantity = payload.quantity;
+		state.total = calculateTotal(state);
+
 		return {
 			...state,
-			quantity: action.payload.quantity,
-			total: calculateTotal(state),
 		};
+
+		// hatalı kod, state async çalıştığında state önce güncelleyip sonra {... state} şeklinde virtual doma gönderiyoruz
+		// return {
+		// 	...state,
+		// 	quantity: action.payload.quantity,
+		// 	total: calculateTotal(state),
+		// };
 	} else if (action.type === 'TimesChanged') {
+		const payload = action.payload as { id: number; times: number };
+
+		state.times = payload.times;
+		const total = calculateTotal(state);
+
 		return {
 			...state,
-			times: action.payload.times,
-			total: calculateTotal(state),
+			total: total,
 		};
 	} else if (action.type === 'ItemAdded') {
+		const payload = action.payload as CouponItem;
+
+		console.log('ItemAdded', action);
+
+		const exists = state.items.find((x) => x.id === payload.id);
+
+		if (!exists) {
+			state.items.push(payload);
+		} else {
+			console.log('aynısından var');
+		}
+
 		return {
 			...state,
-			items: state.items,
 			total: calculateTotal(state),
+		};
+	} else if (action.type === 'ItemSelected') {
+		const payload = action.payload as { selectedId: number };
+
+		console.log('ItemAdded', action);
+
+		state.selectedId = payload.selectedId;
+
+		return {
+			...state,
 		};
 	} else {
 		return { ...state };
